2ccaf9b35873aabffcd33ba5845c748762f37591
Mohamadreza Mosalli <mohamadrezamosalli@gmail.com>
2019-04-06 20:59:39 +0430

    set static files

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import bodyParser from 'body-parser';
 import { createServer } from 'http';
 import express from 'express';
 import morgan from 'morgan';
+import path from 'path';
 
 export default class Core {
     // define properties
@@ -26,6 +27,9 @@ export default class Core {
     }
 
     configuration() {
+        // set static file url
+        this.app.use('/static', express.static(path.resolve('./', 'public')));
+
         // body parser
         this.app.use(bodyParser.json());
         this.app.use(bodyParser.urlencoded({ extended: true }));
